2a130ced6c237536c0769ac27e9ccc411ef12ff1
DY <dinar784@gmail.com>
2024-11-17 19:42:54 -0300

    корзина

--- HelloDjango/mypages/views.py
+++ HelloDjango/mypages/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+from .models import ProductPage
+
+
+def add_to_cart(request, product_id):
+    """Добавление товара в корзину."""
+    try:
+        product = ProductPage.objects.get(id=product_id)
+    except ProductPage.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Товар не найден'}, status=404)
+
+    cart = request.session.get('cart', {})
+
+    # Получаем первое изображение товара
+    first_image = product.images.first()  # Извлекаем первое связанное изображение
+    image_url = first_image.image.url if first_image and first_image.image else None
+
+    # Добавляем товар в корзину
+    if str(product_id) in cart:
+        cart[str(product_id)]['quantity'] += 1
+    else:
+        cart[str(product_id)] = {
+            'name': product.title,
+            'price': str(product.price),
+            'quantity': 1,
+            'image': image_url
+        }
+
+    request.session['cart'] = cart
+    return JsonResponse({'status': 'success', 'message': 'Товар добавлен в корзину'})
+
+
+def view_cart(request):
+    """Просмотр содержимого корзины"""
+    cart = request.session.get('cart', {})
+    cart_total = sum(float(item['price']) * item['quantity'] for item in cart.values())
+    return render(request, 'mypages/cart.html', {'cart': cart, 'cart_total': cart_total})
+
+
+def remove_from_cart(request, product_id):
+    """Удалить товар из корзины"""
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+
+    request.session['cart'] = cart
+    return redirect('view_cart')  # Возвращаем на страницу корзины
