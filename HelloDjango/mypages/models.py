e9f1717be41514b0cb66bc64367e15497586713a
DY <dinar784@gmail.com>
2024-11-17 11:54:38 -0300

    страница каталога

--- HelloDjango/mypages/models.py
+++ HelloDjango/mypages/models.py
@@ -36,19 +36,27 @@ class CatalogPage(Page):
 
     def get_context(self, request):
         context = super().get_context(request)
-        context['best_sellers'] = ProductPage.objects.filter(is_promotional=True)[:6]
-        context['manej'] = ProductPage.objects.filter(category=CategoryPage.objects.filter(id=1689).first())
-        context['pazly'] = ProductPage.objects.filter(category=CategoryPage.objects.filter(id=1690).first())
-        context['podstavki'] = ProductPage.objects.filter(category=CategoryPage.objects.filter(id=3293).first())
-        context['panno'] = ProductPage.objects.filter(category=CategoryPage.objects.filter(id=1691).first())
-        context['upakovka'] = ProductPage.objects.filter(category=CategoryPage.objects.filter(id=1693).first())
-        context['rukodelie'] = ProductPage.objects.filter(category=CategoryPage.objects.filter(id=1694).first())
 
-        context['home_page'] = HomePage.objects.first()  # Получаем страницу главной
+        # Используем `prefetch_related` и `select_related` для оптимизации запросов
+        categories = CategoryPage.objects.filter(id__in=[1689, 1690, 3293, 1691, 1693, 1694]).prefetch_related('products')
+        category_map = {category.id: category for category in categories}
+
+        # Получаем продукты для каждой категории
+        context['best_sellers'] = ProductPage.objects.filter(is_promotional=True)[:6]
+        context['manej'] = category_map.get(1689).products.all() if 1689 in category_map else []
+        context['pazly'] = category_map.get(1690).products.all() if 1690 in category_map else []
+        context['podstavki'] = category_map.get(3293).products.all() if 3293 in category_map else []
+        context['panno'] = category_map.get(1691).products.all() if 1691 in category_map else []
+        context['upakovka'] = category_map.get(1693).products.all() if 1693 in category_map else []
+        context['rukodelie'] = category_map.get(1694).products.all() if 1694 in category_map else []
+
+        # Заранее загружаем страницы для меню
+        context['home_page'] = HomePage.objects.first()
         context['catalog_page'] = self
         context['opt_page'] = OptPage.objects.first()
-        context['news_page'] = NewsPage.objects.first() or None
-        context['about_page'] = AboutPage.objects.first()  # Получаем страницу "О нас"
+        context['news_page'] = NewsPage.objects.first()
+        context['about_page'] = AboutPage.objects.first()
+
         return context
 
     class Meta:
